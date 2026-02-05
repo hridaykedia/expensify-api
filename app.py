e302bf0e359562afd982a5b87bca46c73322ea83
Hriday Kedia <64588629+hridaykedia@users.noreply.github.com>
2026-02-05 17:14:31 +0530

    Update email address in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import curl_cffi.requests as requests
 
 app = Flask(__name__)
 
-EMAIL = "snabulsi@dimagi.com"
+EMAIL = "hkedia@dimagi-associate.com"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Accept": "application/json,text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
